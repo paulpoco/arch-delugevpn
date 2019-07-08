5af25662bde92e175fc28a0e0578f1c6de6fe0be
Paulpoco <paulpoco@gmail.com>
2019-07-08 13:29:43 -0700

    call new pacman updater script
    binhex committed on Feb 14
    commit 53bfc2b3e05a85d6fc643271f64edb281e270249

--- build/root/install.sh
+++ build/root/install.sh
@@ -3,15 +3,7 @@
 # exit script if return code != 0
 set -e
 
-# resetting to live repo and using pacman for this app.
-echo 'Server = http://mirror.bytemark.co.uk/archlinux/$repo/os/$arch' > /etc/pacman.d/mirrorlist
-echo 'Server = http://archlinux.mirrors.uk2.net/$repo/os/$arch' >> /etc/pacman.d/mirrorlist
 
-# download fresh package databases from the server
-pacman -Fyy --noconfirm
-
-# sync package databases for pacman
-pacman -Syyu --noconfirm
 
 # build scripts
 ####
@@ -27,6 +19,9 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
 
 # pacman packages
 ####
+
+# call pacman db and package updater script
+source /root/upd.sh
 
 # define pacman packages
 # following needed for flextget are unzip unrar python2-twisted python2-pip nano gcc pkg-config freetype2
